456db414e96acd461ab5478974f30ffa4f947305
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2019-03-03 01:43:01 -0800

    Adding default code for the case when the user doesn't have AVX2

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const simdjson = require('./build/Release/simdjson.node');
-module.exports = simdjson;
+const simdjson_ = require('./build/Release/simdjson.node');
+if (simdjson.hasAVX2()) {
+    const simdjson = simdjson_;
+    module.exports = simdjson;
+} else {
+    // If the user doesn't have AVX2, we use the default JSON
+    const simdjson = JSON;
+    simdjson["isValid"] = (str) => {
+        try {
+            simdjson.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    };
+    module.exports = simdjson;
+}
--- simdjson/main.cpp
+++ simdjson/main.cpp
@@ -1,8 +1,29 @@
 #include <napi.h>
+
+#ifdef __AVX2__
 #include "bindings.h"
+namespace simdjson {
+  Napi::Boolean hasAVX2(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    return Napi::Boolean::New(env, true);
+  }
+}
+#else
+namespace simdjson {
+  Napi::Boolean hasAVX2(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    return Napi::Boolean::New(env, false);
+  }
+}
+#endif
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+  exports.Set("hasAVX2", Napi::Function::New(env, simdjson::hasAVX2));
+#ifdef __AVX2__
   return simdjson::Init(env, exports);
+#else
+  return exports;
+#endif
 }
 
 NODE_API_MODULE(simdjson, InitAll)
