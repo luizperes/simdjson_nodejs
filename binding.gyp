a40d03d9ebf8c8d5134b9f7407b6450a41b5ecea
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2019-03-03 04:13:45 -0800

    A lot of trouble for no much...

--- binding.gyp
+++ binding.gyp
@@ -1,15 +1,71 @@
-{
-    "targets": [{
-        "target_name": "simdjson",
-        "default_configuration": "Release",
-        "cflags!": [ "-fno-exceptions" ],
-        "cflags_cc!": [ "-fno-exceptions", "-std=gnu++0x", "-std=gnu++1y" ],
-        "cflags_cc+": ["-march=native", "-std=c++17"],
-         "sources": [
-            "simdjson/main.cpp"
-        ],
-        'include_dirs': [
-            "<!@(node -p \"require('nan').include\")"
-        ]
-    }]
+{       
+  "variables": {
+    "avx": "",
+    "avx2": "",
+  },    
+
+  "targets": [
+    {    
+      "target_name": "simdjson",
+      'include_dirs': ["<!@(node -p \"require('nan').include\")"],
+      "sources": ["simdjson/nonavx2.cpp"],    
+      "defines": [],
+      "conditions": [
+        ["OS=='mac'", {          
+          "xcode_settings": {
+            'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
+            'CLANG_CXX_LIBRARY': 'libc++',
+            'MACOSX_DEPLOYMENT_TARGET': '10.7',            
+          },
+          "conditions": [
+            ["avx2 == 'true'", {
+              "sources": ["simdjson/src/simdjson.cpp", "simdson/binding.cpp"],
+              "xcode_settings": {
+                "OTHER_CFLAGS": [
+                  "-mavx2"
+                ]              
+              }              
+            }],
+            ["avx == 'true'", {
+              "sources": ["simdjson/src/simdjson.cpp", "simdson/binding.cpp"],
+              "xcode_settings": {
+                "OTHER_CFLAGS": [
+                  "-mavx"
+                ]              
+              }              
+            }]
+          ]
+        }],
+        ["OS=='linux'", {          
+          'cflags!': [ '-fno-exceptions' ],
+          'cflags_cc!': [ '-fno-exceptions' ],
+          "conditions": [
+            ["avx2 == 'true'", {
+              "sources": ["simdjson/src/simdjson.cpp", "simdson/binding.cpp"],
+              'cflags_cc': [ '-mavx2' ],
+            }],
+            ["avx == 'true'", {
+              "sources": ["simdjson/src/simdjson.cpp", "simdson/binding.cpp"],
+              'cflags_cc': [ '-mavx' ],
+            }]
+          ]
+        }],
+        ["OS=='win'", {
+          'msvs_settings': {
+            'VCCLCompilerTool': { 'ExceptionHandling': 1 },
+          },                    
+          "conditions": [
+            ["avx2 == 'true'", {
+              "sources": ["simdjson/src/simdjson.cpp", "simdson/binding.cpp"],
+              'cflags_cc': [ '-mavx2' ],
+            }],
+            ["avx == 'true'", {
+              "sources": ["simdjson/src/simdjson.cpp", "simdson/binding.cpp"],
+              'cflags_cc': [ '-mavx' ],
+            }]
+          ]
+        }]
+      ]
+    }
+  ]
 }
--- simdjson/bindings.cpp
+++ simdjson/bindings.cpp
@@ -1,99 +1,120 @@
-#include "bindings.h"
+#include <nan.h>
+#include "src/simdjson.h"
 
-bool simdjson::isValid(std::string_view p) {
-  ParsedJson pj = build_parsed_json(p);
-  return pj.isValid();
-}
-
-Napi::Boolean simdjson::IsValidWrapped(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  std::string jstr = info[0].As<Napi::String>();
-  Napi::Boolean returnValue = Napi::Boolean::New(env, simdjson::isValid(jstr));
-  return returnValue;
-}
-
-Napi::Object simdjson::parse(Napi::Env env, std::string_view p) {
-  ParsedJson pj = build_parsed_json(p);
-  if (!pj.isValid()) {
-    Napi::Error::New(env, "Invalid JSON Exception").ThrowAsJavaScriptException();
+  NAN_METHOD(hasAVX2) {
+    auto r = Nan::New(true);
+    info.GetReturnValue().Set(r);
   }
-  ParsedJson::iterator pjh(pj);
-  return simdjson::makeJSONObject(env, pjh).As<Napi::Object>();
-}
 
-Napi::Value simdjson::makeJSONObject(Napi::Env env, ParsedJson::iterator & pjh) {
-  Napi::Value v;
-  if (pjh.is_object()) {
-    Napi::Object obj = Napi::Object::New(env); // {
-    if (pjh.down()) {
-      // must be a string
-      Napi::String key = Napi::String::New(env, pjh.get_string());
-      // :
-      pjh.next();
-      Napi::Value value = simdjson::makeJSONObject(env, pjh); // let us recurse
-      obj.Set(key, value);
-      while (pjh.next()) { // ,
-        key = Napi::String::New(env, pjh.get_string());
-        pjh.next();
-        // :
-        value = simdjson::makeJSONObject(env, pjh); // let us recurse
-        obj.Set(key, value);
-      }
-      pjh.up();
-    }
-    v = obj; // }
-  } else if (pjh.is_array()) {
-    std::vector<Napi::Value> arr;
-    if (pjh.down()) {
-      // [
-      Napi::Value value = simdjson::makeJSONObject(env, pjh); // let us recurse
-      arr.push_back(value);
-      while (pjh.next()) { // ,
-        value = simdjson::makeJSONObject(env, pjh); // let us recurse
-        arr.push_back(value);
-      }
-      pjh.up();
-    }
-    // ]
-    Napi::Array array = Napi::Array::New(env, arr.size());
-    for (std::size_t i{ 0 }; i < arr.size(); i++) array.Set(i, arr[i]);
-    v = array;
-  } else if (pjh.is_string()) {
-    v = Napi::String::New(env, pjh.get_string());
-  } else if (pjh.is_double()) {
-    v = Napi::Number::New(env, pjh.get_double());
-  } else if (pjh.is_integer()) {
-    v = Napi::Number::New(env, pjh.get_integer());
-  } else {
-    switch (pjh.get_type()) {
-      case 't':  {
-        v = Napi::Boolean::New(env, true);
-        break;
-      }
-      case 'f': {
-        v = Napi::Boolean::New(env, false);
-        break;
-      }
-      case 'n': {
-        v = env.Null();
-        break;
-      }
-      default : break;
-    }
+  NAN_METHOD(isValid) {
+      std::string p = *Nan::Utf8String(info[0]);
+      ParsedJson pj = build_parsed_json(p);
+      auto r = Nan::New(pj.isValid());
+      info.GetReturnValue().Set(r);
   }
+
+  NAN_MODULE_INIT(simdjsonInit) {
+      NAN_EXPORT(target, hasAVX2);
+      NAN_EXPORT(target, isValid);
+      //NAN_EXPORT(target, parse);
+  }
+
+  NODE_MODULE(simdjson, simdjsonInit);
+
+
+// bool simdjson::isValid(std::string_view p) {
   
-  return v;
-}
+// }
 
-Napi::Object simdjson::ParseWrapped(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  std::string jstr = info[0].As<Napi::String>();
-  Napi::Object json = simdjson::parse(env, jstr);
-  return json;
-}
+// Napi::Boolean simdjson::IsValidWrapped(const Napi::CallbackInfo& info) {
+//   Napi::Env env = info.Env();
+//   std::string jstr = info[0].As<Napi::String>();
+//   Napi::Boolean returnValue = Napi::Boolean::New(env, simdjson::isValid(jstr));
+//   return returnValue;
+// }
 
-Napi::Object simdjson::Init(Napi::Env env, Napi::Object exports) {
-  exports.Set("isValid", Napi::Function::New(env, simdjson::IsValidWrapped));
-  exports.Set("parse", Napi::Function::New(env, simdjson::ParseWrapped));
-  return exports;
-}
+// Napi::Object simdjson::parse(Napi::Env env, std::string_view p) {
+//   ParsedJson pj = build_parsed_json(p);
+//   if (!pj.isValid()) {
+//     Napi::Error::New(env, "Invalid JSON Exception").ThrowAsJavaScriptException();
+//   }
+//   ParsedJson::iterator pjh(pj);
+//   return simdjson::makeJSONObject(env, pjh).As<Napi::Object>();
+// }
+
+// Napi::Value simdjson::makeJSONObject(Napi::Env env, ParsedJson::iterator & pjh) {
+//   Napi::Value v;
+//   if (pjh.is_object()) {
+//     Napi::Object obj = Napi::Object::New(env); // {
+//     if (pjh.down()) {
+//       // must be a string
+//       Napi::String key = Napi::String::New(env, pjh.get_string());
+//       // :
+//       pjh.next();
+//       Napi::Value value = simdjson::makeJSONObject(env, pjh); // let us recurse
+//       obj.Set(key, value);
+//       while (pjh.next()) { // ,
+//         key = Napi::String::New(env, pjh.get_string());
+//         pjh.next();
+//         // :
+//         value = simdjson::makeJSONObject(env, pjh); // let us recurse
+//         obj.Set(key, value);
+//       }
+//       pjh.up();
+//     }
+//     v = obj; // }
+//   } else if (pjh.is_array()) {
+//     std::vector<Napi::Value> arr;
+//     if (pjh.down()) {
+//       // [
+//       Napi::Value value = simdjson::makeJSONObject(env, pjh); // let us recurse
+//       arr.push_back(value);
+//       while (pjh.next()) { // ,
+//         value = simdjson::makeJSONObject(env, pjh); // let us recurse
+//         arr.push_back(value);
+//       }
+//       pjh.up();
+//     }
+//     // ]
+//     Napi::Array array = Napi::Array::New(env, arr.size());
+//     for (std::size_t i{ 0 }; i < arr.size(); i++) array.Set(i, arr[i]);
+//     v = array;
+//   } else if (pjh.is_string()) {
+//     v = Napi::String::New(env, pjh.get_string());
+//   } else if (pjh.is_double()) {
+//     v = Napi::Number::New(env, pjh.get_double());
+//   } else if (pjh.is_integer()) {
+//     v = Napi::Number::New(env, pjh.get_integer());
+//   } else {
+//     switch (pjh.get_type()) {
+//       case 't':  {
+//         v = Napi::Boolean::New(env, true);
+//         break;
+//       }
+//       case 'f': {
+//         v = Napi::Boolean::New(env, false);
+//         break;
+//       }
+//       case 'n': {
+//         v = env.Null();
+//         break;
+//       }
+//       default : break;
+//     }
+//   }
+  
+//   return v;
+// }
+
+// Napi::Object simdjson::ParseWrapped(const Napi::CallbackInfo& info) {
+//   Napi::Env env = info.Env();
+//   std::string jstr = info[0].As<Napi::String>();
+//   Napi::Object json = simdjson::parse(env, jstr);
+//   return json;
+// }
+
+// Napi::Object simdjson::Init(Napi::Env env, Napi::Object exports) {
+//   exports.Set("isValid", Napi::Function::New(env, simdjson::IsValidWrapped));
+//   exports.Set("parse", Napi::Function::New(env, simdjson::ParseWrapped));
+//   return exports;
+// }
