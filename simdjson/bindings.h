05a5b0c876bc403a7bb58a9be77ed39f56e01f1b
Luiz Peres <luiz_peres10@hotmail.com>
2019-08-02 18:30:43 -0700

    Updating simdjon to version 0.2.0

--- simdjson/bindings.h
+++ simdjson/bindings.h
@@ -3,18 +3,20 @@
 #include "src/simdjson.h"
 
 namespace simdjsonnode {
+  using namespace simdjson;
+
   Napi::Boolean HasAVX2Wrapped(const Napi::CallbackInfo& info);
 
-  bool isValid(std::string_view p);
+  bool isValid(std::string p);
   Napi::Boolean IsValidWrapped(const Napi::CallbackInfo& info);
 
-  Napi::Object parse(Napi::Env env, std::string_view p);
-  Napi::Value makeJSONObject(Napi::Env env, ParsedJson::iterator & pj);
+  Napi::Object parse(Napi::Env env, std::string p);
+  Napi::Value makeJSONObject(Napi::Env env, ParsedJson::Iterator & pj);
   Napi::Object ParseWrapped(const Napi::CallbackInfo& info);
 
   Napi::Object LazyParseWrapped(const Napi::CallbackInfo& info);
   Napi::Value ValueForKeyPathWrapped(const Napi::CallbackInfo& info);
-  Napi::Value findKeyPath(Napi::Env env, std::vector<std::string> subpaths, ParsedJson::iterator & pjh);
+  Napi::Value findKeyPath(Napi::Env env, std::vector<std::string> subpaths, ParsedJson::Iterator & pjh);
 
   Napi::Object Init(Napi::Env env, Napi::Object exports);
 }
